f287bd70d47fed2b061299a1c2e7a74902421c35
JaspreetKaur <JK359@myscc.ca>
2022-12-03 13:06:03 -0500

    add the list of users

--- myapp/src/pages/Home.js
+++ myapp/src/pages/Home.js
@@ -1,10 +1,13 @@
-import {Row,Col, Button} from "antd";
-import { useEffect } from "react";
+import React,{useEffect, useState} from "react";
+import {Row,Col, Button, Divider,Typography} from "antd";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import MyCard from "../Components/MyCard";
 
 export default function Home(props){
 
     const navigate = useNavigate()
+    const {Title} = Typography;
 
     const handleClick =()=>{
         console.log("Something Happened");
@@ -22,6 +25,19 @@ export default function Home(props){
         }
     },[]);
 
+    const [blogs,setBlogs] =useState(null);
+    const [status, setStatus] = useState(false);
+
+    useEffect(()=>{
+        axios.get('https://jsonplaceholder.typicode.com/users').then((response)=>{
+          setBlogs(response.data)
+          setStatus(true)
+        }).catch(()=>{
+          console.log("API call failed...!")
+          setStatus(false)
+        })
+      },[])
+
     return(
         <>
         <Row>
@@ -34,6 +50,25 @@ export default function Home(props){
             <Button type="primary" onClick={handleClick}>Logout</Button>
             </Col>
         </Row>
+        <div>
+        <Row gutter={16} >
+          <Col className='gutter-row'>
+            USERS'S PROFILE
+          </Col>
+        </Row>
+        <Divider orientation="center">PROFILE</Divider>
+        <Row gutter={16} >
+          {status && blogs.map((blogObj)=>{
+            return <MyCard Key={blogObj.id} name={blogObj.name} username={blogObj.username} email={blogObj.email} phone={blogObj.phone} website={blogObj.website}/>
+          })}
+        </Row>
+
+
+    </div>
+    
+
+   
+    
 
         </>
         
